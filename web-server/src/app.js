41f1d747b2e9604ebf969b328539ed092c49c270
Norbert Scheller <norbert@riskident.com>
2020-05-26 19:33:59 +0200

    webServer - partials and 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,35 +1,41 @@
 const path    = require('path')
 const express = require('express')
+const hbs     = require('hbs')
 
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
 
-const publicDir = path.join(__dirname, '../public')
+// Define paths for Express config
+const publicDir   = path.join(__dirname, '../public')
+const viewsDir    = path.join(__dirname, '../templates/views')
+const partialsDir = path.join(__dirname, '../templates/partials')
 
+// Setup handlebars engine and view location
 app.set('view engine', 'hbs')
+app.set('views', viewsDir)
+hbs.registerPartials(partialsDir)
+
+// Setup static dir to serve
 app.use(express.static(publicDir))
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather',
-        name: 'Norbert Scheller'
+        title: 'Weather'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About me',
-        name: 'Norbert Scheller'
+        title: 'About me'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
-        msg: 'This may help you.',
-        name: 'Norbert Scheller'
+        msg: 'This may help you.'
     })
 })
 
@@ -42,7 +48,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
-app.get('/about', )
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        msg: 'Help article not found!',
+        url: req.url
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        msg: 'Page not found!',
+        url: req.url
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
 })

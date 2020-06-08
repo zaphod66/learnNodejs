4afac0d170e20b61cca20e3c92e763299969625a
Norbert Scheller <norbert@riskident.com>
2020-06-08 22:28:29 +0200

    master - webServer merged

---
+++ web-server/public/js/app.js
@@ -0,0 +1,29 @@
+const weatherForm = document.querySelector('form')
+const search      = document.querySelector('input')
+const msg1        = document.querySelector('#msg-1')
+const msg2        = document.querySelector('#msg-2')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    msg1.textContent = "loading..."
+    msg2.textContent = ""
+    
+    const location = search.value
+
+    const url = 'http://localhost:3000/weather?address=' + location
+
+    fetch(url).then( (res) => {
+        res.json().then( (data) => {
+            if (data.error) {
+                msg1.textContent = data.error
+            } else {
+                const text1 = 'Weather for ' + data.data.name + ' is ' + data.data.desc
+                const text2 = 'Temperature is ' + data.data.temp + '°C and feels like ' + data.data.feelslike + '°C'
+
+                msg1.textContent = text1
+                msg2.textContent = text2
+            }
+        })
+    })
+})
---
+++ web-server/src/app.js
@@ -0,0 +1,98 @@
+const path    = require('path')
+const express = require('express')
+const hbs     = require('hbs')
+const geocode = require('./utils/geocode')
+const weather = require('./utils/weather')
+
+console.log(__dirname)
+console.log(path.join(__dirname, '../public'))
+
+const app = express()
+
+// Define paths for Express config
+const publicDir   = path.join(__dirname, '../public')
+const viewsDir    = path.join(__dirname, '../templates/views')
+const partialsDir = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and view location
+app.set('view engine', 'hbs')
+app.set('views', viewsDir)
+hbs.registerPartials(partialsDir)
+
+// Setup static dir to serve
+app.use(express.static(publicDir))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        msg: 'This may help you.'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    const address = req.query.address
+
+    geocode(address, (error, { name, lat, lon } = {} ) => {
+        if (error) {
+            return res.send({ error })
+        }
+        
+        weather(lat, lon, (error, data) => {
+            if (error) {
+                return res.send({ error })
+            }
+            
+            res.send({
+                data,
+                addr: address,
+                name
+            })
+        })
+        
+    })
+})
+
+app.get('/products', (req, res) => {
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        msg: 'Help article not found!',
+        url: req.url
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        msg: 'Page not found!',
+        url: req.url
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})

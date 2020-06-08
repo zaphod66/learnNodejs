5e471955aaf1e45b32ef885e6aacf40b9e713324
Norbert Scheller <norbert@riskident.com>
2020-06-08 22:24:56 +0200

    webServer - button wired

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,9 +1,14 @@
 const weatherForm = document.querySelector('form')
 const search      = document.querySelector('input')
+const msg1        = document.querySelector('#msg-1')
+const msg2        = document.querySelector('#msg-2')
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
+    msg1.textContent = "loading..."
+    msg2.textContent = ""
+    
     const location = search.value
 
     const url = 'http://localhost:3000/weather?address=' + location
@@ -11,9 +16,13 @@ weatherForm.addEventListener('submit', (event) => {
     fetch(url).then( (res) => {
         res.json().then( (data) => {
             if (data.error) {
-                console.log(data.error)
+                msg1.textContent = data.error
             } else {
-                console.log(data.data)
+                const text1 = 'Weather for ' + data.data.name + ' is ' + data.data.desc
+                const text2 = 'Temperature is ' + data.data.temp + '°C and feels like ' + data.data.feelslike + '°C'
+
+                msg1.textContent = text1
+                msg2.textContent = text2
             }
         })
     })
